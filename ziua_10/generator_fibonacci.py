17fd4a97a4ab36d55339da4b49e23c00aba2d551
plescanmarian14-gif <plescanmarian14@gmail.com>
2026-09-15 21:02:19 +0300

    Delete ziua_10 directory

--- ziua_10/generator_fibonacci.py
+++
@@ -1,7 +0,0 @@
-def fibonaci(lim):
-    a,b=0,1
-    while a<=lim:
-        yield a
-        a,b=b,a+b
-for num in fibonaci(50):
-    print(num,end=" ")
--- ziua_10/main.py
+++
@@ -1,5 +0,0 @@
-persoane = [("Ana", 25), ("Bogdan", 19), ("Elena", 30)]
-sortare=sorted(persoane,key=lambda p: p[1])
-print(sortare)
-filtru=list(filter(lambda p: p[1]>19,persoane))
-print(filtru)
